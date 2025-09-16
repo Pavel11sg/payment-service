e8ba58bbdc416ccf162105e333a080e794c0948a
Pavel Shahoika <pavel11sg@gmail.com>
2025-09-16 18:39:54 +0300

    first commit

---
+++ src/test/java/com/example/tasks/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.tasks.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
---
+++ src/test/java/com/example/tasks/paymentservice/TestPaymentServiceApplication.java
@@ -0,0 +1,10 @@
+package com.example.tasks.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(PaymentServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+}
